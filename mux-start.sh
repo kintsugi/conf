4e8afea2983bff203df840cbc2b929636512c614
Kyle Salter <kyle.salter@aero.org>
2017-07-13 10:55:25 -0700

    mux start

--- mux-start.sh
+++ mux-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+tmux has || tmux start \; new-session -d 'sleep 1'
 # start default tmuxinator profiles, run on creation of iterm2 hotkey window
 tmuxinator start --attach=false config 
 tmuxinator start --attach=false cheatsheet
